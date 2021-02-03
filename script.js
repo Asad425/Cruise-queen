b77213aec1f202fc879c533a78ccf6ddf84f8143
Asad425 <asadshanto310@gmail.com>
2021-02-03 19:40:15 +0600

    fifth

--- script.js
+++ script.js
@@ -11,4 +11,23 @@ function plusMinus(whichClass,whichOne){
          newQuantity=quantity-1;
     }
     document.getElementById(whichClass+'-input').value=newQuantity;
+    priceCalculator();
+}
+
+function priceCalculator(){
+    const firstClassQuantity=getQuantity('firstClass');
+    const economyClassQuantity=getQuantity('economy');
+
+    const subTotal=(firstClassQuantity*150)+(economyClassQuantity*100);
+    const tax=subTotal*0.1;
+    const totalPrice=subTotal+tax;
+    document.getElementById('subTotal').innerText='$'+totalPrice;
+    document.getElementById('taxTotal').innerText='$'+tax;
+    document.getElementById('totalPrice').innerText='$'+totalPrice;
+
+}
+
+function getQuantity(category){
+    const quantity=parseInt(document.getElementById(category+'-input').value);
+    return quantity;
 }
